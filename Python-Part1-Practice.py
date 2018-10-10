ed1e368f80dd6603eb73bd7bb5d1a4771a3fcbe8
allualexander333 <44014958+allualexander333@users.noreply.github.com>
2018-10-10 14:07:31 +0530

    Update Python-Part1-Practice.py

---
+++ Python-Part1-Practice.py
@@ -0,0 +1,25 @@
+# TODO: 1. Write a Python function that takes a sequence of numbers and determines if all the numbers are different from each other.
+
+import math
+import random
+
+v1 = int(input("Enter first Number:" ))
+v2 = int(input("Enter second Number:" ))
+v3 = int(input("Enter third Number:" ))
+
+def notequal(v1,v2,v3):
+    if(v1==v2):
+        result = "Values are Equal"
+        return result
+    elif(v2==v3):
+        result = "Values are Equal"
+        return result
+    elif(v1==v3):
+        result = "Values are Equal"
+        return result
+    else:
+        result = "Values are not equal"
+        return result
+result=notequal(v1,v2,v3)
+print("The provided ",result)
+
